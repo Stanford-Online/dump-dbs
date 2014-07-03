951b29244f78964af6d00dedbb5d7a9f6b53122e
Sef Kloninger <sef@kloninger.com>
2014-07-03 16:27:14 -0600

    cleaner config, multiple recipeints, stdout if no mail

--- large_files.py
+++ large_files.py
@@ -1,54 +1,57 @@
 #!/usr/bin/env python
+#
+# config.yml should have a stanza that looks like this:
+#
+# large_files:
+#    host: MONGO_FULLY_QUALIFIED_DOMAIN_NAME
+#    port: MONGO_PORT
+#    user: MONGO_USER_NAME
+#    password: MONGO_PASSWORD
+#    db: MONGO_DB_NAME
+#    size: 10000000
+#    sender: admin@foo.com
+#
 
 import sys
 import os
 import subprocess
 from collections import OrderedDict
-
-import smtplib
-from email.mime.text import MIMEText
-
 import json
 import yaml
 import ordered_yaml
-
-myname = sys.argv[0]
-
-sender = "sef@stanford.edu"
-if len(sys.argv) > 1:
-    recipient = sys.argv[1]
-else:
-    recipient = "sef@stanford.edu"
+import smtplib
+from email.mime.text import MIMEText
 
 CONFIG_FILENAME = "config.yml"
-DEFAULT_TARGET_DIR = "/data/dump"
-
+myname = sys.argv[0]
+recipients = sys.argv[1:]
 
 # Worker Functions
 
-def query_mongo(config, db):
+def query_mongo(config):
+    config['query'] = ("db.fs.files.aggregate(["
+                       "{$match: {length: {$gt: %s }}},"
+                       "{$project: {filename:1,length:1, _id:0}},"
+                       "{$sort: {length:-1}}"
+                       "]).toArray()") \
+        % config['size']
     cmd_template = ("echo '{query}' "
                     "| mongo --quiet {host}:{port}/{db} "
                     "-u {user} -p {password}")
-    cmd = [cmd_template.format(**config[db])]
+    cmd = [cmd_template.format(**config)]
     output = subprocess.check_output(cmd, shell=True)
-    result_dict = json.loads(output)
-    return result_dict
+    resultlist = json.loads(output)
+    return resultlist
 
 
-def email_report(results):
-    info("sending mail to {}".format(recipient))
-
-    body = "There are {} large files in GridFS.\n\n".format(len(results))
-    for entry in results:
-        body += "{}: {}\n".format(entry['filename'], entry['length'])
+def email_report(body, result_count, sender):
     msg = MIMEText(body)
-    msg['Subject'] = "Large GridFS Files: {}".format(len(results))
+    msg['Subject'] = "Large GridFS Files: {}".format(result_count)
     msg['From'] = sender
-    msg['To'] = recipient
+    msg['To'] = ",".join(recipients)
 
     s = smtplib.SMTP('localhost')
-    s.sendmail(sender, [recipient], msg.as_string())
+    s.sendmail(sender, recipients, msg.as_string())
     s.quit()
 
 
@@ -65,16 +68,26 @@ def error(msg):
 # Main
 
 def main():
-    config_file = open(CONFIG_FILENAME, "r")
-    config = yaml.load(config_file, Loader=ordered_yaml.OrderedDictYAMLLoader)
+    with open(CONFIG_FILENAME, "r") as config_file:
+        config = yaml.load(config_file,
+                           Loader=ordered_yaml.OrderedDictYAMLLoader)
     if 'large_files' not in config:
-        error("large_file_query stanza not found in config.yml")
+        error("large_files stanza not found in configuration file")
         sys.exit(1)
-    result_dict = query_mongo(config, 'large_files')
-    results = result_dict['result']
-    info("found {} files".format(len(results)))
-    if len(results) > 0:
-        email_report(results)
+    bigs = query_mongo(config['large_files'])
+
+    sizeMB = int(config['large_files']['size']) / 1e6
+    report = "There are {} large files in GridFS over {} MB.\n\n" \
+        .format(len(bigs), sizeMB)
+    for big in bigs:
+        report += "{}: {}\n".format(big['filename'], big['length'])
+    if len(recipients):
+        info("found {} files".format(len(bigs)))
+        info("sending mail to {}".format(recipient))
+        email_report(report, len(bigs), config['large_files']['sender'])
+    else:
+        sys.stdout.write(report)
 
 if __name__ == "__main__":
     main()
+
